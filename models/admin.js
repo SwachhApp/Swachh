64b361968dfba626866f24c2c2d6fbb09844b4d0
agnipanda <pandaagni@outlook.com>
2019-10-03 03:30:14 +0530

    setup admin schema and added login and signup api for admin

--- models/admin.js
+++ models/admin.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var userSchema = new Schema({
+var adminSchema = new Schema({
     name: {type:String},
     email: {type:String,unique:true},
     password: {type:String}
 });
 
-var userModel = mongoose.model('user',userSchema);
-module.exports = userModel;
+var adminModel = mongoose.model('admin',adminSchema);
+module.exports = adminModel;
--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,61 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var userModel = require('../models/user');
+var adminModel = require('../models/admin');
 var bcrypt = require('bcrypt');
 
 const saltRounds = 10;
+
+router.post('/adminsignup',function(req, res, next) {
+    adminModel.findOne({"email":req.body.email}, function(error, result) {
+        if(result == null){
+            bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+                var adminmodel = new adminModel({
+                    name:req.body.name,
+                    email:req.body.email,
+                    password: hash
+                });
+        
+                adminmodel.save(function(err,data) {       
+                    if(err){
+                        console.log(err)
+                        res.status(200).send({auth:false,msg:"server error"});
+                    }
+                    else{
+                        res.status(200).send({auth:true,msg:"success",id:data.id});
+                    }
+                });
+            });
+        }
+        else{
+            res.send({auth:false,msg:"user already exists"})
+        }
+    });
+});
+
+router.post('/adminlogin',function(req, res, next) {
+    console.log(req.body);
+    adminModel.findOne({"email":req.body.email}, function(error, data) {
+        console.log(data);
+        // console.log(req.body.password, data.password);
+        
+        if(data != null){
+            console.log(req.body.password, data.password);
+            
+            bcrypt.compare(req.body.password, data.password, function(err, result) {
+                if(result === true){
+                    res.status(200).send({auth:true,id:data.id});
+                }
+                else{
+                    res.send({auth:false});
+                }
+            });
+        }
+        else{
+            res.send({auth:false});
+        }
+    });
+})
 
 router.post('/signup',function(req, res, next) {
 
@@ -19,16 +71,16 @@ router.post('/signup',function(req, res, next) {
         
                 usermodel.save(function(err,data) {       
                     if(err){
-                        res.status(500).send({msg:"server error"});
+                        res.status(500).send({auth:false, msg:"server error"});
                     }
                     else{
-                        res.status(200).send({msg:"success"});
+                        res.status(200).send({auth:true, id:data.id, msg:"success"});
                     }
                 });
             });
         }
         else{
-            res.send({msg:"user already exists"})
+            res.send({auth:false,msg:"user already exists"})
         }
     });
 });
