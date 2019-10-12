9f6ba6e1c83c8b5e53f3f48e10c4e4126bc4d5d3
sandipbhuyan <sandipbhuyan@gmail.com>
2019-10-12 10:56:47 +0530

    Make a column for modifying the password for admin

--- models/admin.js
+++ models/admin.js
@@ -5,6 +5,7 @@ var adminSchema = new Schema({
     name: { type:String },
     email: { type:String, unique: true },
     password: { type:String },
+    passwordModified: { type: Boolean, default: false}, // A admin has to change the password once after creation. Make value true after this. 
     createdAd: { type:Date, default: Date.now() },
     lastLogin: { type: Date, default: Date.now() },
     vendorsCreated: { type: Number, default: 0 },
